ca4a80e9f4a4038bed37c6fc65a8cece6ee1a628
eden <xiaoyumqyz@gmail.com>
2021-04-09 17:51:13 +1200

    fix tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,21 @@ module.exports = {
   },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      'xs': {'max': '599px'},
+      'sm': {'min': '600px', 'max': '959px'},
+      'md': {'min': '960px', 'max': '1279px'},
+      'lg': {'min': '1280px', 'max': '1919px'},
+      'xl': {'min': '1920px', 'max': '5000px'},
+      'lt-sm': {'max': '599px'},
+      'lt-md': {'max': '959px'},
+      'lt-lg': {'max': '1279px'},
+      'lt-xl': {'max': '1919px'},
+      'gt-xs': {'min': '600px'},
+      'gt-sm': {'min': '960px'},
+      'gt-md': {'min': '1280px'},
+      'gt-lg': {'min': '1920px'},
+    }
   },
   variants: {
     // outline: ['responsive', 'focus-within', 'focus'],
